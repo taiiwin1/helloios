3f823cada8986335272be46a1d03f39aeb79b2ee
Tai Nguyen <tainnguyen2@gmail.com>
2026-01-12 13:27:57 -0800

    Initial Commit

---
+++ HelloIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloIOS
+//
+//  Created by Tai Nguyen on 1/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
